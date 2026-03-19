cbc71b9d2e7e6c5ee556b98eac18b82f37f71f5c
Jas <jasper.aleksander.nyman@gmail.com>
2026-03-18 23:38:15 -0700

    Revert "Merge branch 'main' of https://github.com/jasper-nyman/ultimatum"
    This reverts commit 265ec345b59627b70ccc60e9e11a7a48ffcec9f6, reversing
    changes made to affc4a34e3f9029d9fa3d339bc0fc91956f27990.

--- Assets/Scenes/Team/Valerie/Scripts/Inventory.cs
+++
@@ -1,7 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Inventory : MonoBehaviour
-{
-    public List<ItemData> items;
-}
--- Assets/Scenes/Team/Valerie/Scripts/ItemData.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Item Data", menuName = "Scriptable Objects/Item Data")]
-
-public class ItemData : ScriptableObject
-{
-    public Sprite sprite;
-    [TextArea] public string description; 
-}
