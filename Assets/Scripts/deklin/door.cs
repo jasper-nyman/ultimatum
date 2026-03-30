e9ad5ddb1753b3edc9a0a8e211d61b621ccaf3c8
MUKILTEO\1725525 <Deklinfis@outlook.com>
2026-03-30 12:11:18 -0700

    door ai

--- Assets/Scripts/deklin/door.cs
+++ Assets/Scripts/deklin/door.cs
@@ -12,33 +12,38 @@ public class door : MonoBehaviour, IInteractable
 
     public Animator anim;
     public doorstate state;
-    public AudioClip opennoise, closenoise;
+    public AudioClip openNoise, closeNoise;
 
+
+    
+    
     public void Interact()
     {
         if (state == doorstate.close)
         {
             open();
+         
         }
         else
         {
             close();
+            
         }
 
-        FindFirstObjectByType<NavMeshSurface>().BuildNavMesh();
+        
     }
-
+    
     public void open()
     {
         anim.SetBool("isopen", true);
         state = doorstate.open;
-        GetComponent<AudioSource>().PlayOneShot(opennoise);
+        GetComponent<AudioSource>().PlayOneShot(openNoise);
     }
 
     public void close()
     {
         anim.SetBool("isopen", false);
         state = doorstate.close;
-        GetComponent<AudioSource>().PlayOneShot(closenoise);
+        GetComponent<AudioSource>().PlayOneShot(closeNoise);
     }
 }
