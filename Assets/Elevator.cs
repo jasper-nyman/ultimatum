b3b2c6a9734c36891bfe668b9067ad4fbc0c48b1
MUKILTEO\1725364 <1725364@mukilteo.wednet.edu>
2026-04-24 13:15:52 -0700

    Testing

--- Assets/Elevator.cs
+++ Assets/Elevator.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-public class Elevator : MonoBehaviour
+public class ElevatorI : MonoBehaviour
 {
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.CompareTag("Player"))
         {
-            collision.transform.position = new Vector3(50f, 6f, 7.75f);
+            collision.transform.position = new Vector3(13.5f, 6f, 7.75f);
         }
     }
 }
